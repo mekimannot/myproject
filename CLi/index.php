fcc3e597146016fea0afb265b8e91acac656ca56
mekimannot <mekiermena72@gmail.com>
2022-11-20 14:30:50 +0300

    adding some text to index

--- CLi/index.php
+++ CLi/index.php
@@ -52,7 +52,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clinic Management system hello</title>
+    <title>Clinic Management system skdhgksdjhkg</title>
     <link rel="stylesheet" href="sty.css">
     <link rel="stylesheet" type="text/css" href="bootstrap.css">
     <style>
